6529b060cc197f50bd1e506a2fe113b9495c37f7
Rimantovas <rimas.pov@gmail.com>
2022-10-27 22:27:50 +0300

    deleted pip controller

--- jitsi_meet/ios/Classes/JitsiViewController.swift
+++ jitsi_meet/ios/Classes/JitsiViewController.swift
@@ -5,7 +5,7 @@ class JitsiViewController: UIViewController {
     
     @IBOutlet weak var videoButton: UIButton?
     
-    fileprivate var pipViewCoordinator: PiPViewCoordinator?
+    // fileprivate var pipViewCoordinator: PiPViewCoordinator?
     fileprivate var jitsiMeetView: JitsiMeetView?
     
     var eventSink:FlutterEventSink? = nil
@@ -51,7 +51,7 @@ class JitsiViewController: UIViewController {
                                      with coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransition(to: size, with: coordinator)
         let rect = CGRect(origin: CGPoint.zero, size: size)
-        pipViewCoordinator?.resetBounds(bounds: rect)
+        // pipViewCoordinator?.resetBounds(bounds: rect)
     }
     
     func openJitsiMeet() {
@@ -84,12 +84,12 @@ class JitsiViewController: UIViewController {
         // Enable jitsimeet view to be a view that can be displayed
         // on top of all the things, and let the coordinator to manage
         // the view state and interactions
-        pipViewCoordinator = PiPViewCoordinator(withView: jitsiMeetView)
-        pipViewCoordinator?.configureAsStickyView(withParentView: view)
+        // pipViewCoordinator = PiPViewCoordinator(withView: jitsiMeetView)
+        // pipViewCoordinator?.configureAsStickyView(withParentView: view)
         
         // animate in
         jitsiMeetView.alpha = 0
-        pipViewCoordinator?.show()
+        // pipViewCoordinator?.show()
     }
     
     func closeJitsiMeeting(){
@@ -99,7 +99,7 @@ class JitsiViewController: UIViewController {
     fileprivate func cleanUp() {
         jitsiMeetView?.removeFromSuperview()
         jitsiMeetView = nil
-        pipViewCoordinator = nil
+        // pipViewCoordinator = nil
         //self.dismiss(animated: true, completion: nil)
     }
 }
@@ -126,12 +126,12 @@ extension JitsiViewController: JitsiMeetViewDelegate {
         mutatedData?.updateValue("onConferenceTerminated", forKey: "event")
         self.eventSink?(mutatedData)
         
-        DispatchQueue.main.async {
-            self.pipViewCoordinator?.hide() { _ in
-                self.cleanUp()
-                self.dismiss(animated: true, completion: nil)
-            }
-        }
+        // DispatchQueue.main.async {
+        //     self.pipViewCoordinator?.hide() { _ in
+        //         self.cleanUp()
+        //         self.dismiss(animated: true, completion: nil)
+        //     }
+        // }
         
     }
     
@@ -140,9 +140,9 @@ extension JitsiViewController: JitsiMeetViewDelegate {
         var mutatedData = data
         mutatedData?.updateValue("onPictureInPictureWillEnter", forKey: "event")
         self.eventSink?(mutatedData)
-        DispatchQueue.main.async {
-            self.pipViewCoordinator?.enterPictureInPicture()
-        }
+        // DispatchQueue.main.async {
+            // self.pipViewCoordinator?.enterPictureInPicture()
+        // }
     }
     
     func exitPictureInPicture() {
